f2e479c8b67655e409461dce28330a3177c840cf
serhii <romanchuk.sss22121999@gmail.com>
2021-02-06 16:21:23 +0200

    13.02 migration to new types(TodolistType, TaskType) - refactoring tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,27 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        {
+            name: '@storybook/addon-storysource',
+            options: {
+                rule: {
+                    test: [/\.stories\.tsx?$/],
+                    //include: [path.resolve(__dirname, '../src')], // You can specify directories
+                },
+                loaderOptions: {
+                    prettierConfig: {
+                        printWidth: 80,
+                        singleQuote: false,
+                        options: {parser: 'typescript'}
+                    },
+                },
+            },
+        },
+    ]
 }
--- src/state/tasks-reducer/tasks-reducer.test.ts
+++ src/state/tasks-reducer/tasks-reducer.test.ts
@@ -1,7 +1,7 @@
 import {v1} from 'uuid';
-import {TasksType} from '../../App';
-import {addTaskAC, changeStatusTaskAC, changeTitleTaskAC, removeTaskAC, tasksReducer} from './tasks-reducer';
+import {addTaskAC, changeStatusTaskAC, changeTitleTaskAC, removeTaskAC, tasksReducer, TasksType} from './tasks-reducer';
 import {addTodolistAC, removeTodolistAC} from '../todolist-reducer/todolists-reducer';
+import {TaskPriorities, TaskStatusesType} from '../../api/task-api';
 
 let todoListId1: string;
 let todoListId2: string;
@@ -14,14 +14,74 @@ beforeEach(() => {
 
    startState = {
       [todoListId1]: [
-         {id: '1', title: 'CSS', isDone: true},
-         {id: '2', title: 'HTML', isDone: true},
-         {id: '3', title: 'JS', isDone: false},
-         {id: '4', title: 'REDUX', isDone: false},
+         {
+            id: '1',
+            title: 'CSS',
+            status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId1
+         },
+         {
+            id: '2',
+            title: 'HTML',
+            status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId1
+         },
+         {
+            id: '3', title: 'JS', status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId1
+         },
+         {
+            id: '4',
+            title: 'REDUX',
+            status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId1
+         },
       ],
       [todoListId2]: [
-         {id: '1', title: 'Spartacus', isDone: false},
-         {id: '2', title: 'Tom and Jerry', isDone: false},
+         {id: '1',
+            title: 'Spartacus',
+            status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId2},
+         {id: '2',
+            title: 'Tom and Jerry',
+            status: TaskStatusesType.New,
+            addedDate: '',
+            deadline: '',
+            description: '',
+            order: 0,
+            priority: TaskPriorities.Middle,
+            startDate: '',
+            todoListId: todoListId2},
       ]
    }
 })
@@ -49,11 +109,11 @@ test('task of correct todolist should be added', () => {
    expect(endState[todoListId2].length).toBe(2);
 
    expect(endState[todoListId1][0].title).toBe(taskTitle);
-   expect(endState[todoListId1][0].isDone).toBe(false);
+   expect(endState[todoListId1][0].status).toBe(TaskStatusesType.New);
 });
 
 test('task of correct todolist should be have another status', () => {
-   let newStatus = true;
+   let newStatus = TaskStatusesType.Completed;
 
    let action = changeStatusTaskAC('1', newStatus, todoListId2);
 
@@ -62,7 +122,7 @@ test('task of correct todolist should be have another status', () => {
    expect(endState[todoListId1].length).toBe(4);
    expect(endState[todoListId2].length).toBe(2);
 
-   expect(endState[todoListId2][0].isDone).toBe(newStatus);
+   expect(endState[todoListId2][0].status).toBe(newStatus);
 });
 
 test('task of correct todolist should be have another title', () => {
@@ -84,7 +144,7 @@ test('when add todolist we should have tasks for this todolist in TASKS', () => 
    let endState: TasksType = tasksReducer(startState, action);
 
    let keys = Object.keys(endState);
-   let newKey = keys.find(key => key !== todoListId1 && key !== todoListId2 );
+   let newKey = keys.find(key => key !== todoListId1 && key !== todoListId2);
 
    if (!newKey) {
       throw new Error('new key should be added');
