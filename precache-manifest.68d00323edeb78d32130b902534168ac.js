0f1bdd044e0d1a4be4095474b0025b7ee24417a8
serhii <romanchuk.sss22121999@gmail.com>
2021-03-02 16:08:39 +0200

    Updates

---
+++ precache-manifest.68d00323edeb78d32130b902534168ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9fc481e3c114f420ea10691a2113c8",
+    "url": "/TODOLIST/index.html"
+  },
+  {
+    "revision": "c131de4423226a71feb4",
+    "url": "/TODOLIST/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a5cf477a902f7c8f2915",
+    "url": "/TODOLIST/static/js/2.9ef8851f.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/TODOLIST/static/js/2.9ef8851f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c131de4423226a71feb4",
+    "url": "/TODOLIST/static/js/main.d7d5ac53.chunk.js"
+  },
+  {
+    "revision": "f5e1b1800e04f9a3a42e",
+    "url": "/TODOLIST/static/js/runtime-main.87016690.js"
+  }
+]);
